387fb56af7ccb2ad1532d46619ef8f916bca360f
shelly9611 <61039754+shelly9611@users.noreply.github.com>
2023-06-04 14:00:37 +0800

    Add files via upload

---
+++ ProGitForProgrammers/Form1.cs
@@ -0,0 +1,15 @@
+namespace ProGitForProgrammers
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello world!", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
